5da760b9cf6c35d2cd9bb475cac4823362af8bbd
MOGAU GODFREY NGWATLE <ngwatlegodfrey@gmail.com>
2022-10-24 09:59:15 +0200

    removed hard coded code

---
+++ src/MergeFiles.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class MergeFiles {
+
+    private String pathRoot;
+    private String outputFile;
+
+    public MergeFiles(String pathRoot, String outputFile) {
+        this.pathRoot = pathRoot;
+        this.outputFile = outputFile;
+    }
+
+    public void findFilesAndConcatenate() throws IOException {
+        File dir = new File(pathRoot);
+        File fileOutputFileDir = new File(dir, outputFile);
+        PrintWriter pw = new PrintWriter(new FileWriter(fileOutputFileDir));
+        String[] files = dir.list();
+
+        for (int i = 0; i < files.length; i++) {
+
+            File f = new File(dir, files[i]);
+
+            if (f.isDirectory()) {
+                findFilesAndConcatenate();
+            } else {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String readLine = br.readLine();
+
+                while (readLine != null) {
+                    pw.println(readLine);
+                    readLine = br.readLine();
+                }
+
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            pw.close();
+            pw.flush();
+        }
+    }
+
+}
